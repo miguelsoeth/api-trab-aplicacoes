81dda90316743da277bac2c53f34ed05f99acd60
Miguel Soeth <miguelsoeth@gmail.com>
2024-06-24 23:05:34 -0300

    Removido console.log

--- src/api/middlewares/authMiddleware.js
+++ src/api/middlewares/authMiddleware.js
@@ -7,7 +7,6 @@ const authMiddleware = (req, res, next) => {
   }
   jwt.verify(token, 'secret-key', (err, decoded) => {
     if (err) {
-      console.log("ERRO", err)
       return res.status(500).json({ message: 'Failed to authenticate token' });
     }
     req.userId = decoded.user._id; // Attach user ID to request object
